deec816cb2a8a7258d03c31047327af1370137e4
MaheshB77 <mbansode7696@gmail.com>
2026-01-04 12:12:05 +0530

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Mahesh Bansode on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
