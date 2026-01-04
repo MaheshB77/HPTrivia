5976feb4f8ce13beabef141760b6f70b0ead781f
MaheshB77 <mbansode7696@gmail.com>
2026-01-05 00:43:40 +0530

    Added play button with some animations

--- HPTrivia/views/ContentView.swift
+++ HPTrivia/views/ContentView.swift
@@ -5,11 +5,12 @@
 //  Created by Mahesh Bansode on 04/01/26.
 //
 
-import SwiftUI
 import AVKit
+import SwiftUI
 
 struct ContentView: View {
     @State private var player: AVAudioPlayer!
+    @State private var playButtonAnimation = false
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -28,17 +29,58 @@ struct ContentView: View {
                     } animation: { _ in
                         .linear(duration: 80)
                     }
+
+                VStack {
+                    Image(systemName: "wand.and.stars")
+                        .imageScale(.large)
+                        .font(.largeTitle)
+                        .padding(.bottom, 20)
+                    Text("HP")
+                        .font(.custom("PartyLetPlain", size: 80))
+                        .padding(.bottom, -20)
+                    Text("Trivia")
+                        .font(.custom("PartyLetPlain", size: 72))
+
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+
+                    Button {
+
+                    } label: {
+                        Text("Play")
+                            .font(.largeTitle)
+                            .padding(.horizontal)
+                    }
+                    .buttonStyle(.glass)
+                    .scaleEffect(playButtonAnimation ? 1.1 : 0.9)
+                    .onAppear {
+                        withAnimation(
+                            .easeInOut(duration: 1.6).repeatForever()
+                        ) {
+                            playButtonAnimation.toggle()
+                        }
+                    }
+                    .transition(.offset(y: geo.size.height / 4))
+
+                    Spacer()
+                }
+                .padding(.top, 72)
             }
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
         .onAppear {
-            playBackgroundMusic()
+            //            playBackgroundMusic()
         }
     }
-    
+
     private func playBackgroundMusic() {
-        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        let sound = Bundle.main.path(
+            forResource: "magic-in-the-air",
+            ofType: "mp3"
+        )
         player = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
         player.numberOfLoops = -1
         player.play()
