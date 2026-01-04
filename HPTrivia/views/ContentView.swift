7f77f8fb7add92148c8b02efab652f577631abe2
MaheshB77 <mbansode7696@gmail.com>
2026-01-04 18:21:12 +0530

    Added bg image and animation on home screen

--- HPTrivia/views/ContentView.swift
+++ HPTrivia/views/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                Image(.hogwarts)
+                    .resizable()
+                    .frame(width: geo.size.width * 4, height: geo.size.height)
+                    .padding(.top, 4)
+                    .border(.blue)
+                    .phaseAnimator([false, true]) { content, phase in
+                        content
+                            .offset(
+                                x: phase
+                                    ? geo.size.width / 1.1
+                                    : -geo.size.width / 1.1
+                            )
+                    } animation: { _ in
+                        .linear(duration: 80)
+                    }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
